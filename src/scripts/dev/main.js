f45876318ca496cc845d08c11eb524aea92edc93
Anastasia Shapenkova <wondernasty@yandex.ru>
2024-09-28 16:22:06 +0500

    first commit

---
+++ src/scripts/dev/main.js
@@ -0,0 +1,9 @@
+'use strict'
+
+//Шапка сайта при скролле
+document.addEventListener('scroll', function() {
+    let headerLink = document.querySelector('.page-header');
+    window.scrollY > 50 
+        ? headerLink.classList.add('scrolled') 
+        : headerLink.classList.remove('scrolled');
+});

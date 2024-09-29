152785f0cda94fecd8028f3aa901c93762261d4e
Anastasia Shapenkova <wondernasty@yandex.ru>
2024-09-29 15:05:45 +0500

    делала пару блоков(слайдер и статистика)

--- src/scripts/dev/main.js
+++ src/scripts/dev/main.js
@@ -7,3 +7,25 @@ document.addEventListener('scroll', function() {
         ? headerLink.classList.add('scrolled') 
         : headerLink.classList.remove('scrolled');
 });
+
+//Swiper
+const swipers = document.querySelectorAll(".js-swiper");
+swipers.forEach(function (swpr) {
+    new Swiper(swpr, {
+        updateOnWindowResize: true,
+        slidesPerView: "auto",
+        freeMode: true,
+        spaceBetween: 0,
+        speed: 500,
+        grabCursor: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true
+        },
+        navigation: {
+        nextEl: ".swiper-arrow-next",
+        prevEl: ".swiper-arrow-prev",
+        disabledClass: "arrow--disabled"
+        }
+    });
+});
